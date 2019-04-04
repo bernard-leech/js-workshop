1d91decf43df93af52d3cf7a0f3ebe34f6b3aaea
Conor O'Driscoll <conor o driscoll@c02qlan1fvh5.crunch.internal>
2019-04-04 11:33:47 +0100

    Create Money Component

---
+++ src/js/components/presentational/1-atoms/money/Money.js
@@ -0,0 +1,31 @@
+// @flow
+import React from 'react'
+
+import type { MoneyType } from './Money.types'
+
+function numberWithSeparator(number, separator) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+}
+
+function Money (props: MoneyType) {
+  const {
+    moneyValue,
+    currencySymbol = '£',
+    separator = ',',
+    decimal = '.'
+  } = props
+  
+  const integerValue = Math.floor(moneyValue)
+  const fractionalValue = Math.round((moneyValue - integerValue) * 100)
+  const integerValueWithCommas = numberWithSeparator(integerValue, separator);
+  
+  const money = `${currencySymbol}${integerValueWithCommas}${decimal}${fractionalValue}`
+  
+  return (
+    <div className='number' style={{fontVariantNumeric: 'tabular-nums', fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'}}>
+      {money}
+    </div>
+  )
+}
+
+export default Money
