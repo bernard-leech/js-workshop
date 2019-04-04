08d898161b8ee9b3f4335c9b65fdd1865c98a157
Ben Herbert <ben.herbert@crunch.co.uk>
2019-04-04 11:29:41 +0100

    BH -  Status Pill Component

---
+++ src/js/components/presentational/1-atoms/StatusPill.js
@@ -0,0 +1,27 @@
+// @flow
+import React from 'react'
+import classNames from 'classnames'
+
+import type { StatusPillType } from './StatusPill.types'
+
+function StatusPill (props: StatusPillType) {
+  const {
+    status,
+    children
+  } = props
+
+  const statusPillClasses = classNames('status-pill',
+    {
+      'status-pill--error': status === 'error',
+      'status-pill--success': status === 'success'
+    }
+  )
+
+  return (
+    <div className={statusPillClasses}>
+      {children}
+    </div>
+  )
+}
+
+export default StatusPill
